6e30a369f1fe63fb0f8ec24bc9dce75ef9f3fd19
GATEWAYGROUP\Andlib.Naqvi <andlibmehndi.naqvi@thegatewaycorp.com>
2019-08-13 17:07:31 +0530

    Detail api for question and added 404 shortcut

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 import json
 
 from .models import Question
@@ -30,3 +31,14 @@ def questions_view(request):
             question = serializer.save()
             return Response(QuestionSerializer(question).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def question_detail_view(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == 'GET':
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
+    elif request.method == 'PATCH':
+        raise NotImplementedError("PATCH currently not supported")
+    elif request.method == 'DELETE':
+        raise NotImplementedError("DELETE currently not supported")
